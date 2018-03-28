b0ee155639fd674e17cac078a2a721cc7e026890
Travis Nelson <travis.o.nelson@gmail.com>
2018-03-27 21:38:51 -0600

    Changed color to teal

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.write('{\"color\": \"black\"}')
+        self.response.write('{\"color\": \"teal\"}')
 
 
 app = webapp2.WSGIApplication([
