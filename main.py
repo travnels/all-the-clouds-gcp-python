ef36574fb53f285217e2dfc98ca623880313c66d
Travis Nelson <travis.o.nelson@gmail.com>
2018-03-28 19:08:07 -0600

    pink

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.write('{\"color\": \"lightgrey\"}')
+        self.response.write('{\"color\": \"pink\"}')
 
 
 app = webapp2.WSGIApplication([
